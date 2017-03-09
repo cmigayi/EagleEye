d79dc00effc1c33be4a241e59a3f042aacda5b7d
cilo <migayicecil@gmail.com>
2017-03-09 18:02:42 +0300

    added sum to text view

--- app/src/main/java/com/example/cilo/eagleeye/MainActivity.java
+++ app/src/main/java/com/example/cilo/eagleeye/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cilo.eagleeye;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView totalTV = (TextView) findViewById(R.id.total);
+
+        int sum = 5 + 90;
+
+        totalTV.setText(sum);
+
     }
 }
